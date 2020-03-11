24b55056df0cb0eb1e0a032742d5ea23853df6fc
edogawadoras1234@gmail.com <edogawadoras1234@gmail.com>
2020-03-11 10:02:25 +0700

    Add project files.

---
+++ bài 1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bài_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+
+            {
+
+                using (SqlConnection connection =
+
+                    new SqlConnection(@"Server=DESKTOP-2SVCGMM\SQLEXPRESS"))
+
+                {
+
+                    connection.Open();
+
+                }
+
+                MessageBox.Show("Mo va dong co so du lieu thanh cong.");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("Loi khi mo  ket noi: " + ex.Message);
+
+            }
+        }
+    }
+}
